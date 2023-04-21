6ff0b97bdfe6cf42bb1adaa682e9fcc585997032
marthinsalakory <marthinsalakory11@gmail.com>
2023-04-21 23:00:24 +0900

    Mengubah link mulai sekarang

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <a href="index.php" class="hero-logo" data-aos="zoom-in"><img src="assets/img/buku.png" width="60" alt=""></a>
     <h1 data-aos="zoom-in">Selamat Datang di<br>Kamus Bahasa Alune Indonesia</h1>
     <!-- <h2 data-aos="fade-up">We are team of talented designers making websites with Bootstrap</h2> -->
-    <a data-aos="fade-up" data-aos-delay="200" href="kata-indonesia.php" class="btn-get-started scrollto">Mulai Sekarang</a>
+    <a data-aos="fade-up" data-aos-delay="200" href="otomatis-indonesia.php" class="btn-get-started scrollto">Mulai Sekarang</a>
   </div>
 </section><!-- End Hero -->
 <?php include 'footer.php' ?>
